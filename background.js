1ddff182429de4b182d33a21bb1eb4c9572c5f2c
Gilbert Haro <gilbertharocode@gmail.com>
2023-09-28 01:23:57 -0700

    console testing

--- background.js
+++ background.js
@@ -1,11 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+  if (activeTab.url.includes('?wcmmode=edit')) {
+    // Page is already in edit mode; you can implement a different action
+    console.log('Page is in edit mode.');
+    // Add your logic for handling edit mode here
+  } else {
     // Open page in edit mode
-    chrome.tabs.update(tab.id, {url: tab.url + "?wcmmode=edit"});
-  });
-  
-  
-  
-  // Open page properties
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.update(tab.id, {url: tab.url + "/jcr:content/properties.html"});
-  });
+    chrome.tabs.update(activeTab.id, { url: activeTab.url + '?wcmmode=edit' });
+  }
+});
+
+// chrome.browserAction.onClicked.addListener((tab) => {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     const tab = tabs[0];
+//     if (tab && tab.url) {
+//       if (!tab.url.includes('?wcmmode=edit')) {
+//         chrome.tabs.update(tab.id, { url: tab.url + '?wcmmode=edit' });
+//       }
+//     }
+//   });
+// });
--- popup.js
+++ popup.js
@@ -1,8 +1,14 @@
-document.getElementById("editModeButton").addEventListener("click", function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "openEditMode"});
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // Your code here
+  document
+    .getElementById('editModeButton')
+    .addEventListener('click', function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'openEditMode' });
+      });
+    });
+
+  // Add more event listeners for other buttons
 });
 
 // Add more event listeners for other buttons
-
